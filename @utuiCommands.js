a8d895d5fc72760c5456e680aba8df42e978b312
Rui Machado <ruipedromachado@hotmail.com>
2019-06-17 13:09:37 +0100

    Update @utuiCommands.js

--- @utuiCommands.js
+++ @utuiCommands.js
@@ -144,6 +144,17 @@ utui.define.show();
  * privacy manager
  */
 
-
+//
+// enable/disable v2
+//
 utui.privacy_management.util.enableV2()
 utui.privacy_management.util.disableV2()
+
+//
+// copy between profiles
+//
+//source profile
+var _tmp_pm_data = JSON.stringify(utui.data.privacy_management);
+
+//Destination profile
+utui.data.privacy_management = JSON.parse(_tmp_pm_data);
