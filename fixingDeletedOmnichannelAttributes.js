987c1f3b24727c09d3710434df0d4921d72b4e3e
Rui Machado <ruipedromachado@hotmail.com>
2019-06-23 22:22:41 +0100

    Create fixingDeletedOmnichannelAttributes.js

---
+++ fixingDeletedOmnichannelAttributes.js
@@ -0,0 +1,30 @@
+/*
+
+* "5125"    : "omni_attr"
+* "5094"    : "price"
+* "5092"    : "product_id"
+* "5093"    : "quantity"
+
+
+"5119"      : "omni_attr"
+"5040"      : "mail"        <-----
+"5064"      : "another"
+
+
+5235
+5236
+
+"type": "omnichannel"
+
+-------------------------------------------------------
+save only is enough
+break point at "as_core.js"-->"saveVersion"-->"return...."
+
+copy "dataObj"
+adjust
+copy adjustment back to "dataObj" = JSON.stringify({{adjustment}})
+
+complete reload and check
+
+ */
+
